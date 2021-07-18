7d1492a4175061390e445edc99ce2d53c738980a
willaimRodrigo <billtechnew@gmail.com>
2021-07-18 19:17:35 -0300

    add classe conta

---
+++ src/main/kotlin/digibankone/conta.kt
@@ -0,0 +1,16 @@
+package digibankone
+
+import java.math.BigDecimal
+
+class conta (
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+){
+    fun deposito(valor: BigDecimal) {
+
+    }
+    fun saque(valor:BigDecimal) {
+
+    }
+}
--- src/main/kotlin/digibankone/pessoa.kt
+++ src/main/kotlin/digibankone/pessoa.kt
@@ -6,12 +6,15 @@ class Pessoa {
     var cpf: String= "123.456.789-00"
     private set
 
+    constructor()
+
+    fun pessoaInfo()= "$nome e $cpf"
+
     }
 
 fun main() {
     val willaim= Pessoa()
 
-    println(willaim.nome)
-    println(willaim.cpf)
+    println(willaim.pessoaInfo())
 
 }
